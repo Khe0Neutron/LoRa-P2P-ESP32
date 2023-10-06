b4a8aa948f162430b0258b85c4b2422a6f675b9b
Yimmy <yimmy.cruz.electronic@gmail.com>
2023-10-06 15:28:32 -0500

    Programa listo

--- main/esp32lora.c
+++ main/esp32lora.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include "string.h"
-//#include "freertos/FreeRTOS.h"
-//#include "freertos/task.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "ssd1306.h"
-//#include "lora.h"
+#include "lora.h"
 
 #define MESSAGE_LENGTH 240
 
 SSD1306_t screen;
-//TaskHandle_t xHandleRXTask;
-//uint8_t msg[MESSAGE_LENGTH];
-//int packets = 0;
-//int rssi = 0;
+uint8_t msg[MESSAGE_LENGTH];
+int packets = 0;
+int rssi = 0;
 
 void screen_init() {
   i2c_master_init(&screen, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
@@ -28,7 +27,7 @@ void screen_print(char * str, int page) {
   ssd1306_display_text(&screen, page, str, strlen(str), false);
 }
 
-/* void task_rx(void *p) {
+void task_rx(void *p) {
   char packets_count[64];
   char rssi_str[64];
   int len;
@@ -37,8 +36,6 @@ void screen_print(char * str, int page) {
     while(lora_received()) {
       len = lora_receive_packet(msg, MESSAGE_LENGTH);
       msg[len] = 0;
-
-      printf("Receive msg: %s, len: %d\n", msg, len);
 
       rssi = lora_packet_rssi();
       packets++;
@@ -50,20 +47,11 @@ void screen_print(char * str, int page) {
     }
     vTaskDelay(1);
   }
-} */
-
-/* void send_msg(char * msg, int size) {
-  printf("send packet: %s\n", msg);
-  vTaskSuspend(xHandleRXTask);
-  lora_send_packet((uint8_t *)msg, size);
-  vTaskResume(xHandleRXTask);
-  printf("packet sent!\n");
 }
 
 void task_tx(void *p) {
   for(;;) {
-    send_msg("Ping!", 5);
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    
   }
 }
 
@@ -72,13 +60,13 @@ void lora_config_init() {
   lora_init();
   lora_set_frequency(915e6);
   lora_enable_crc();
-  xTaskCreate(&task_rx, "task_rx", 2048, NULL, 5, &xHandleRXTask);
+  xTaskCreate(&task_rx, "task_rx", 2048, NULL, 5, NULL);
   xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
-} */
+}
 
 void app_main(void) {
   screen_init();
   screen_clear();
-  screen_print("Hola mundo!", 0);
-  //lora_config_init();
+  // screen_print("Hola mundo!", 0);
+  lora_config_init();
 }
